0228a2ca8d9ed0aa957ad923839e8a8122115211
GTA <hhfhfh@gmail.com>
2022-10-06 00:39:41 -0300

    Initial commit

---
+++ src/main/java/com/library/regexm3u/RegexM3U.java
@@ -0,0 +1,44 @@
+package com.library.regexm3u;
+
+import java.io.File;
+
+public class RegexM3U {
+
+    public   void Getinfo(String texto){
+
+        Compile_TEXTO(texto);
+
+    }
+
+
+    public void  Getinfo(File  FileTXT){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    private  void  Compile_TEXTO(String str){
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
