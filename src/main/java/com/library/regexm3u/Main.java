cc41dca99a1abb2268d689cc470b7d366377af28
GTA <hhfhfh@gmail.com>
2022-10-06 22:03:09 -0300

    Atualizado

--- src/main/java/com/library/regexm3u/Main.java
+++ src/main/java/com/library/regexm3u/Main.java
@@ -1,124 +1,40 @@
 package com.library.regexm3u;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-
-        JSONObject object = new JSONObject();
-        JSONArray ja = new JSONArray();
-
-        ArrayList list = new ArrayList();
-
-
-
+    public static void main(String[] args) throws IOException {
 
 
         String texto = "#EXTINF: -1 tvg-logo='https://upload.wikimedia.org/wikipedia/commons/f/fe/CARTOON_NETWORK_logo.png' tvg-name='globo' tvg-link='http://cdn.tvwebplay.top/live/teste123/teste123/101.ts' group-title='Desenhos', Cartoon\n" +
                 "http://cdn.tvwebplay.top/live/teste123/teste123/101.ts\n" +
                 "#EXTINF:-1 tvg-id=\"\" tvg-logo=\"https://minhalista-files.nyc3.digitaloceanspaces.com/iptv-list.jpg\" group-title=\"Futebol Ao Vivo\",Jogos Ao Vivo 5\n" +
-                "http://aovivo.futeboltv.com:8081/live/15/chunks.m3u8?LISTASGLAU\n"+
+                "http://aovivo.futeboltv.com:8081/live/15/chunks.m3u8?LISTASGLAU\n" +
                 "#EXTINF: -1 tvg-logo='https://upload.wikimedia.org/wikipedia/commons/f/fe/CARTOON_NETWORK_logo.pnh' tvg-name='Sbt' tvg-link='http://cdn.tvwebplay.top/live/teste123/teste123/101.ts' group-title='Desenhos', Cartoon\\n\" +\n" +
                 "http://cdn.tvwebplay.top/live/teste123/teste123/101.ts";
 
 
-        Matcher list_found = Pattern.compile("#EXTINF:.*\\n*.*").matcher(texto);
-
-
-        //"(tvg-logo=.?.*?)[|']"
+        RegexM3U regexM3U = new RegexM3U();
 
 
 
+        File file =  new File("canais.txt");
 
-        //Matcher tvg_name_count = Pattern.compile("tvg-name=['|\"].*?['!\"]").matcher(texto);
+      JSONArray jsonArray =  regexM3U.Getinfo(new File("canais.txt"));
 
-
-        //Matcher tvg_logo_count = Pattern.compile("tvg-logo=['|\"].*?['!\"]").matcher(list_found.group());
+      System.out.println(jsonArray);
 
 
 
 
-
-
-        while (list_found.find()){
-
-            Matcher tvg_logo_count = Pattern.compile("tvg-logo=['|\"].*?['!\"]").matcher(list_found.group());
-
-            if(tvg_logo_count.find()){
-                //get logo texto
-                String logo = tvg_logo_count.group()
-                        .replaceAll("tvg-logo=['\"]","")
-                        .replaceAll("['\"]","");
-
-                System.out.println(logo);
-
-
-
-
-            }
-
-
-
-
-
-            Matcher tvg_name_count_option1 = Pattern.compile("tvg-name=['|\"].*?['!\"]").matcher(list_found.group());
-            Matcher tvg_name_count_option2 = Pattern.compile(",.*").matcher(list_found.group());
-
-
-            if(tvg_name_count_option1.find()){
-                String name = tvg_name_count_option1.group()
-
-                        .replaceAll("tvg-name=['\"]","")
-                        .replaceAll("['\"]","");
-
-
-
-                System.out.println(name);
-
-            }else  if(tvg_name_count_option2.find()){
-
-
-
-
-                System.out.println(tvg_name_count_option2.group().replaceAll(",","").trim());
-
-
-            }
-
-
-
-
-
-            Matcher t = Pattern.compile("\\n(http|https).*").matcher(list_found.group());
-
-            if(t.find()) {
-
-                System.out.println(t.group().trim());
-
-            }
-
-
-
-
-
-
-
-
-        }
-
-
-
-
-
-
-
+        //System.out.println(regexM3U.Getinfo(texto));
 
 
 
     }
+
+
 }
--- src/main/java/com/library/regexm3u/RegexM3U.java
+++ src/main/java/com/library/regexm3u/RegexM3U.java
@@ -1,44 +1,115 @@
 package com.library.regexm3u;
 
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegexM3U {
+    /**
+     * Pega informaçôes da lista em jsonAray
+     * @param str_texto
+     * @return JsonArray
+     */
+    public JSONArray Getinfo(String str_texto) {
 
-    public   void Getinfo(String texto){
-
-        Compile_TEXTO(texto);
-
-    }
-
-
-    public void  Getinfo(File  FileTXT){
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-    private  void  Compile_TEXTO(String str){
-
+        return Compilar_TEXTO(str_texto);
 
 
     }
 
+    /**
+     * Pega informaçôes da lista em jsonAray
+     * Converte aquivo txt em string
+     * @param FileTXT
+     * @return JsonArray
+     * @throws IOException
+     */
+    public JSONArray Getinfo(File FileTXT) throws IOException {
+
+        String texto = FileUtils.readFileToString(FileTXT, "UTF-8");
 
 
+        return Compilar_TEXTO(texto);
+
+    }
+
+    /**
+     * Estrair nome,logos,link,etc e converte em JsonArray
+     * @param texto
+     * @return JsonArray
+     */
+    private JSONArray Compilar_TEXTO(String texto) {
 
 
+        JSONArray jsonArray = new JSONArray();
 
 
+        Matcher list_found = Pattern.compile("#EXTINF:.*\\n*.*").matcher(texto);
 
 
+        while (list_found.find()) {
+            JSONObject object = new JSONObject();
+
+            Matcher tvg_logo_count = Pattern.compile("tvg-logo=['|\"].*?['!\"]").matcher(list_found.group());
+
+            if (tvg_logo_count.find()) {
+                //get logo texto
+                String logo = tvg_logo_count.group()
+                        .replaceAll("tvg-logo=['\"]", "")
+                        .replaceAll("['\"]", "");
+
+
+                jsonArray.put(object.put("img", logo));
+
+
+            }
+
+
+            Matcher tvg_name_count_option1 = Pattern.compile("tvg-name=['|\"].*?['!\"]").matcher(list_found.group());
+            Matcher tvg_name_count_option2 = Pattern.compile(",.*").matcher(list_found.group());
+
+
+            if (tvg_name_count_option1.find()) {
+                String name = tvg_name_count_option1.group()
+
+                        .replaceAll("tvg-name=['\"]", "")
+                        .replaceAll("['\"]", "");
+
+
+                jsonArray.put(object.put("nome", name));
+
+
+            } else if (tvg_name_count_option2.find()) {
+
+                String name = tvg_name_count_option2.group().replaceAll(",", "").trim();
+                jsonArray.put(object.put("nome", name));
+
+
+            }
+
+
+            Matcher t = Pattern.compile("\\n(http|https).*").matcher(list_found.group());
+
+            if (t.find()) {
+                String link_canal = t.group().trim();
+
+
+                jsonArray.put(object.put("canal", link_canal));
+
+            }
+
+
+        }
+
+        return jsonArray;
+
+
+    }
 
 
 }
